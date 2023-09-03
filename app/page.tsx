5595e774c6a75b412b7d44e8c6204af50f87e751
Alex Carvalho <alexander_sck@live.ca>
2023-09-03 11:06:56 -0400

    fix: css

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div id="container" className="flex flex-col items-center space-y-4">
         <div id="title" className="w-full flex flex-row space-x-7">
           <div className="flex flex-col grow justify-center">
-            <h1 className="text-3xl md:text-4xl font-serif tracking-wider">Alexander Carvalho</h1>
+            <h1 className="text-3xl md:text-4xl font-semibold">Alexander Carvalho</h1>
             <Banner />
           </div>
         </div>
