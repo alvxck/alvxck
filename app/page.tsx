ecd93d8154fbe85438b9bd42f7065a59dd219664
Alex Carvalho <alexander_sck@live.ca>
2024-03-27 19:15:11 -0400

    fix: spelling

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         </div>
         <div id="about" className="w-full space-y-4">
           <p className="text-md text-neutral-400">Hi – my name is <span className="font-medium text-white">Alex</span>.</p>
-          <p className="text-md text-neutral-400">I&apos;m a <span className="font-medium text-white">Software Engineer</span> from from Toronto interested in <span className="font-medium text-white">finance technology</span> and building <span className="font-medium text-white">sports cars</span>.</p>
+          <p className="text-md text-neutral-400">I&apos;m a <span className="font-medium text-white">Software Engineer</span> from Toronto interested in <span className="font-medium text-white">finance technology</span> and building <span className="font-medium text-white">sports cars</span>.</p>
           <p className="text-md text-neutral-400">Incoming <span className="font-medium text-white">Data Engineering</span> Intern at the <a href="https://www.rbcroyalbank.com/personal.html" target="blank" id="job" className="font-medium text-white">Royal Bank of Canada</a>. Previously at <a href="https://www.ansys.com/" target="blank" id="job" className="font-medium text-white">Ansys</a> and <a href="https://www.geotab.com/" target="blank" id="job" className="font-medium text-white">Geotab</a>.</p>
         </div>
         <div id='links' className="w-full">
